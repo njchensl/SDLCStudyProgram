b3d07d16b994cf4f914c4d852ff572ce5a10b264
frche1699 <frche1699@8CXRP52.Student.UGDSB.ED>
2019-11-28 14:34:14 -0500

    question class finished

---
+++ src/sdlc/Question.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdlc;
+
+/**
+ *
+ * @author frche1699
+ */
+public class Question {
+
+    private String question;
+    private String options[];
+    private String correctOption;
+
+    public Question(String q, String[] ops, String correct) {
+        question = q;
+        options = ops;
+        correctOption = correct;
+    }
+
+    public String getOption(int n) {
+        return options[n];
+    }
+
+    public void setOption(int n, String s) {
+        options[n] = s;
+    }
+    
+    public void setOptions(String s[]) {
+        options = s;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String s) {
+        question = s;
+    }
+
+    public boolean isCorrect(String s) {
+        return s.equals(correctOption);
+    }
+
+    public String getCorrectOption() {
+        return correctOption;
+    }
+
+    public void setCorrectOption(String s) {
+        correctOption = s;
+    }
+
+}
