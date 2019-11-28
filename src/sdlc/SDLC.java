18af3ffb521b9ab49c79b2465fadcfa405aa5a3e
frche1699 <frche1699@8CXRP52.Student.UGDSB.ED>
2019-11-28 14:46:02 -0500

    finished test

--- src/sdlc/SDLC.java
+++ src/sdlc/SDLC.java
@@ -8,6 +8,7 @@ package sdlc;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -18,11 +19,21 @@ public class SDLC {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
         // show the JFrame
         JFrame f = new JFrame("SDLC") {
             {
+                try {
+                    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                        if ("Windows".equals(info.getName())) {
+                            javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                            break;
+                        }
+                    }
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {}
                 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                this.setContentPane(new Welcome());
                 this.setPreferredSize(new Dimension(500, 500));
                 this.setResizable(false);
                 this.pack();
@@ -32,5 +43,5 @@ public class SDLC {
             f.setVisible(true);
         });
     }
-    
+
 }
